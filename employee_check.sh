02083277433b94d12b6d3accb558c04d630767a2
sachinlokesh05 <sachin.beee.15@acharya.ac.in>
2020-02-25 15:35:31 +0530

    [sachin]added one more basic program

---
+++ employee_check.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -x
+isFulltime=2
+isHalftime=1
+isAbsent=0
+empRatePerHr=$((RANDOM%2000))
+checkRandom=$((RANDOM%3))
+if [ $checkRandom == $isFulltime ]
+then
+	empWrkHr=8;
+elif [ $checkRandom == $isHalftime ]
+then 
+	empWrkHr=4;
+else
+	empWrkHr=0;
+fi
+salary=$(($empRatePerHr*$empWrkHr))
+echo $salary
+
